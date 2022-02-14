b873c6eaeb4d667b195b2b84090b2f19525238f6
Amal Swaroop Banswal <96770978+Amal-Banswal@users.noreply.github.com>
2022-02-14 18:50:05 +0530

    Add files via upload

---
+++ Output of Assignment-01_(21104120).py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\HP\Downloads\Telegram Desktop\Assignment-01_(21104120).py =
+PYHTON PROGRAM TO FIND AVERAGE OF THREE GIVEN NUMBERS
+Enter the first number: 45
+Enter the second number: 58
+Enter the third number: 71
+Average of  45 ,  58  and  71  is  58.0
+--------------------------------------------------------
+PYTHON PROGRAM TO CALCULATE THE INCOME TAX OF PERSON AFTER SEVERAL DEDUCTION OF MONEY (GROSS INCOME>$30000)
+Please Enter the Gross Income($) 650000
+Please Enter the number of dependents: 4
+The total Income Tax of a person is: $ 753600.0
+--------------------------------------------------------
+PYTHON PROGRAM TO STORE VALUES IN LISTS
+Enter the SID of the student: 21104120
+Enter the NAME of the student: Amal Swaroop Banswal
+Enter the GENDER (M/F): M
+Enter the COURSE NAME of the student: Introduction to Computing
+Enter the CGPA of the student: 8.8
+[21104120, 'Amal Swaroop Banswal', 'M', 'Introduction to Computing', 8.8]
+--------------------------------------------------------
+PYTHON PROGRAM TO ENTER THE MARKS OF FIVE STUDENTS
+Enter the marks of first Student:72
+Enter the marks of second Student: 89
+Enter the marks of third Student: 95
+Enter the marks of fourth Student: 87
+Enter the marks of fifth Student: 99
+The Lists of marks of five students is: 
+[72, 89, 95, 87, 99]
+--------------------------------------------------------
+PYTHON PROGRAM TO PRINT A SPECIFIED LIST IN A MANNER
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+--------------------------------------------------------
