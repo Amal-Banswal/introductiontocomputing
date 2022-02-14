3fd1f2599de20893588ef39973c421bb511eb465
Amal Swaroop Banswal <96770978+Amal-Banswal@users.noreply.github.com>
2022-02-14 18:41:35 +0530

    Add files via upload

---
+++ Output of Assignment-03_21104120.py
@@ -0,0 +1,82 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: D:\Semester 1\Assignment-03(Code)_(21104120).py ===========
+Python program to count the number of occurrences of each word or character in the string entered by the user
+Enter the string: Life is boring and full of Depression.
+Enter the word: full
+Occurences of full is: 1
+--------------------------------------------------------
+python script to print next date of input date with given input
+Day [1-31]: 28
+Month [1-12]: 08
+Year[1800-2025]: 2001
+The next date is: 29/8/2001
+--------------------------------------------------------
+Python program to create a list of tuples with the first element as the number and Second element as the square of the number
+[(1, 1), (2, 4), (3, 9), (4, 16)]
+--------------------------------------------------------
+program to prompt the user for a grade
+Enter grade points: 7
+Your Grade is B and Good Performance
+--------------------------------------------------------
+Python program to print Given pattern
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+--------------------------------------------------------
+python script that repeatedly ask user to enter name and SID of students
+Enter Name: Amal Swaroop Banswal
+Enter SID: 21104120
+Do you want to enter more details[y/n]: y
+Enter Name: Harsh Shukla
+Enter SID: 21104102
+Do you want to enter more details[y/n]: y
+Enter Name: Samit Gupta
+Enter SID: 21104008
+Do you want to enter more details[y/n]: y
+Enter Name: Vivek kumar Rahul
+Enter SID: 21104125
+Do you want to enter more details[y/n]: n
+
+Details of the students: 
+SID	Name
+21104120	Amal Swaroop Banswal
+21104102	Harsh Shukla
+21104008	Samit Gupta
+21104125	Vivek kumar Rahul
+
+Sorted dictionary using student SID: 
+SID	Name
+21104008	Samit Gupta
+21104102	Harsh Shukla
+21104120	Amal Swaroop Banswal
+21104125	Vivek kumar Rahul
+
+Sorted dictionary using student Names: 
+SID	Name
+21104120	Amal Swaroop Banswal
+21104102	Harsh Shukla
+21104008	Samit Gupta
+21104125	Vivek kumar Rahul
+
+Enter the SID of the student: 21104120
+Name of the student is:  Amal Swaroop Banswal
+--------------------------------------------------------
+python program to print Fibonacci sequence and  average of the resultant Fibonacci series
+Enter the terms: 5
+
+The fibonacci series is:
+0 1 1 2 3 
+The average of the fibonacci series:  1.4
+--------------------------------------------------------
+Question 8
+{1, 3, 5, 6, 8}
+{17, 3, 6, 8, 9, 13}
+{1, 2, 4, 5}
+{6, 7, 8, 9, 10}
+{10, 7}
+--------------------------------------------------------
