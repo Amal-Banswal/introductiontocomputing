7632c3226da7ee1e1b8621ec26f045562d864c08
Amal Swaroop Banswal <96770978+Amal-Banswal@users.noreply.github.com>
2022-02-14 18:50:47 +0530

    Add files via upload

---
+++ OUTPUT of assignment-02-(21104120).py
@@ -0,0 +1,48 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:\Semester 1\Assignment-02(Code).py ================
+PYHTON PROGRAM FOR GIVEN INPUT STRING
+The length of the given string is:  35
+Reverse of the given string is:  egaugnal evitisnes esac a si nohtyP
+After slicing a particular string:  a case sensitive
+After replacing:  Python is object oriented language
+Position of 'a' in the string is:  10
+String after removing white spaces:  Pythonisacasesensitivelanguage
+--------------------------------------------------------
+PYTHON PROGRAM TO STORE VALUES IN DIFFERENT VARIABLES WITH THE USE OF STRING FORMATTING PRINT
+Enter Name: Amal Swaroop Banswal
+Enter SID: 21104120
+Enter Department: Electrical
+Enter CGPA: 9.8
+Hey,  Amal Swaroop Banswal  here!
+My SID is  21104120 
+I'm from  Electrical department and my CGPA is  9.8
+--------------------------------------------------------
+PYTHON PROGRAM FOR PERFORMATION OF DIFFERNT OPERATIONS WITH THE HELP OF BIT-WISE OPERATOR
+a =  56
+b =  10
+a & b =  8
+a | b =  58
+a ^ b =  50
+a after left shift with 2 bits:  224
+b after left shift with 2 bits:  40
+a after right shift with 4 bits:  3
+b after right shift with 4 bits:  0
+--------------------------------------------------------
+PYTHON PROGRAM TO FIND THE GREATEST OF THREE NUMBERS
+Enter first no.: 23
+Enter second no.: 56
+Enter third no.: 72
+Greatest of the 3 no. is:  72
+--------------------------------------------------------
+PYTHON PROGRAM TO CHECK IF THE WORD “NAME” IS PRESENT IN THE STRING
+Enter any string: I am learning python.
+No, the string doesn't contains name
+--------------------------------------------------------
+PYTHON PROGRAM FOR GIVEN THREE SIDES TO FORM A TRIANGLE
+Enter first side: 12
+Enter second side: 13
+Enter third side: 5
+No, a triangle can not be formed.
+--------------------------------------------------------
