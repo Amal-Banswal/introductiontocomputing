690fd185110c564cdc653f2e059ed04830558dd3
Amal Swaroop Banswal <96770978+Amal-Banswal@users.noreply.github.com>
2022-02-27 22:43:45 +0530

    Add files via upload

---
+++ Output of Assignment-04_21104120.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: D:\Semester 1\Assignment-04(Code)-(21104120).py ===========
+
+[Question 1]
+
+Enter number of disk in tower of Hanoi:2
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks from Source Tower to Destination Tower is given below:
+
+Move disk 1 from Source Tower to Auxiliary
+Move disk 2 from Source Tower to Destination
+Move disk 1 from Auxiliary to Destination
+
+[Question.2](Recursive)
+The Pascle's Triangle using recursive approach is:
+
+Enter number of rows in Pascle's Triangle:11
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+                                       1  6  15  20  15  6  1                                       
+                                     1  7  21  35  35  21  7  1                                     
+                                   1  8  28  56  70  56  28  8  1                                   
+                                1  9  36  84  126  126  84  36  9  1                                
+                           1  10  45  120  210  252  210  120  45  10  1                            
+
+[Question.2](Iterative)
+The Pascle's Triangle using iterative approach is:
+
+Enter number of rows:5
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+
+[Question.3]
+
+Enter an Integer:12
+Enter another Integer:34
+
+The quotient when 12 is divided by 34 is 0.
+The remainder when 12 is divided by 34 is 12.
+
+Que3.a
+Function is Not-callable
+
+Que3.b
+All result values are NOT 'non-zero'
+
+Que3.c
+Appended (4,5,6) in the values (0,12)
+Values greater than 4 is 12,5,6
+
+Que3.d
+Above result in set form is shown below:
+{12, 5, 6}
+
+Que3.e
+The above set has been converted to immutable.
+
+Que3.f
+Max value from set is 12
+Hash value of 12 is 12
+
+[Question.4]
+
+Hello, My name is Amal and my roll no. is 21104120
+Destructor Called
+
+[Question.5]
+
+Employee Name is Daksh and Salary is 40000 
+Employee Name is Abhay and Salary is 50000 
+Employee Name is Amal and Salary is 60000 
+
+Que.5a
+Daksh salary Updated to 70000
+Employee Name is Daksh and Salary is 70000 
+
+Que.5b
+Employee Abhay's data has been removed.
+
+[Question 6]
+
+          WELCOME TO THE FRIENDSHIP GAME
+
+Enter Player1 name:Harsh
+Enter Player2 name:Vivek
+
+Enter Word by Harsh:Power
+Enter Word by Vivek:Depression
+
+Friendship of Harsh and Vivek is FAKE
+
